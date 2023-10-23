11540695adcb67771f19b4def2056ab9fea164a5
Purna Shrestha <crush9918@gmail.com>
2023-10-23 16:02:32 +0545

    feat: Add previous, next button and the button slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
 const music = document.getElementById("music");
 const playlist = document.getElementById("playlist");
 const playButtons = document.querySelectorAll(".play-button");
+const prevButton = document.getElementById("prev-button");
+const nextButton = document.getElementById("next-button");
+const volumeControl = document.getElementById("volume-control");
+const volumeLabel = document.getElementById("volume-label");
 
 let currentTrackIndex = 0;
 
@@ -15,3 +19,23 @@ const playTrack = (src) => {
   music.src = src;
   music.play();
 };
+prevButton.addEventListener("click", () => {
+  currentTrackIndex--;
+  if (currentTrackIndex < 0) {
+    currentTrackIndex = playButtons.length - 1;
+  }
+  playTrack(playButtons[currentTrackIndex].getAttribute("data-src"));
+});
+
+nextButton.addEventListener("click", () => {
+  currentTrackIndex++;
+  if (currentTrackIndex > playButtons.length - 1) {
+    currentTrackIndex = 0;
+  }
+  playTrack(playButtons[currentTrackIndex].getAttribute("data-src"));
+});
+
+volumeControl.addEventListener("input", () => {
+  music.volume = volumeControl.value;
+  volumeLabel.innerHTML = volumeControl.value;
+});
