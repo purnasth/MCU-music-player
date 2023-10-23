32b898d42e05fc442af3ffc85f289b3e035dd3ed
Purna Shrestha <crush9918@gmail.com>
2023-10-23 16:13:02 +0545

    Convert normal function to arrow function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,67 +1,62 @@
-const music = document.getElementById("music");
-const playlist = document.getElementById("playlist");
-const playButtons = document.querySelectorAll(".play-button");
-const prevButton = document.getElementById("prev-button");
-const nextButton = document.getElementById("next-button");
-const shuffleButton = document.getElementById("shuffle-button");
-const volumeControl = document.getElementById("volume-control");
-const volumeLabel = document.getElementById("volume-label");
+const music = document.getElementById('music');
+const playlist = document.getElementById('playlist');
+const playButtons = document.querySelectorAll('.play-button');
+const prevButton = document.getElementById('prev-button');
+const nextButton = document.getElementById('next-button');
+const shuffleButton = document.getElementById('shuffle-button');
+const volumeControl = document.getElementById('volume-control');
+const volumeLabel = document.getElementById('volume-label');
 
 let currentTrackIndex = 0;
 let isShuffled = false;
 
-playButtons.forEach((button, index) => {
-  button.addEventListener("click", function () {
-    currentTrackIndex = index;
-    playTrack(button.getAttribute("data-src"));
-  });
-});
+playButtons.forEach(button => button.addEventListener('click', () => {
+    currentTrackIndex = Array.from(playButtons).indexOf(button);
+    playTrack(button.getAttribute('data-src'));
+}));
 
-prevButton.addEventListener("click", playPreviousTrack);
-nextButton.addEventListener("click", playNextTrack);
-shuffleButton.addEventListener("click", toggleShuffle);
+prevButton.addEventListener('click', () => playPreviousTrack());
+nextButton.addEventListener('click', () => playNextTrack());
+shuffleButton.addEventListener('click', () => toggleShuffle());
 
-volumeControl.addEventListener("input", updateVolume);
+volumeControl.addEventListener('input', () => updateVolume());
 
-music.addEventListener("ended", function () {
-  playNextTrack();
-});
+music.addEventListener('ended', () => playNextTrack());
 
-function playTrack(src) {
-  music.src = src;
-  music.play();
-}
+const playTrack = src => {
+    music.src = src;
+    music.play();
+};
 
-function playPreviousTrack() {
-  currentTrackIndex =
-    (currentTrackIndex - 1 + playButtons.length) % playButtons.length;
-  playTrack(playButtons[currentTrackIndex].getAttribute("data-src"));
-}
+const playPreviousTrack = () => {
+    currentTrackIndex = (currentTrackIndex - 1 + playButtons.length) % playButtons.length;
+    playTrack(playButtons[currentTrackIndex].getAttribute('data-src'));
+};
 
-function playNextTrack() {
-  if (isShuffled) {
-    currentTrackIndex = getRandomIndex();
-  } else {
-    currentTrackIndex = (currentTrackIndex + 1) % playButtons.length;
-  }
-  playTrack(playButtons[currentTrackIndex].getAttribute("data-src"));
-}
+const playNextTrack = () => {
+    if (isShuffled) {
+        currentTrackIndex = getRandomIndex();
+    } else {
+        currentTrackIndex = (currentTrackIndex + 1) % playButtons.length;
+    }
+    playTrack(playButtons[currentTrackIndex].getAttribute('data-src'));
+};
 
-function toggleShuffle() {
-  isShuffled = !isShuffled;
-  shuffleButton.textContent = isShuffled ? "Shuffle (On)" : "Shuffle (Off)";
-}
+const toggleShuffle = () => {
+    isShuffled = !isShuffled;
+    shuffleButton.textContent = isShuffled ? 'Shuffle (On)' : 'Shuffle (Off)';
+};
 
-function updateVolume() {
-  const volume = volumeControl.value;
-  music.volume = volume;
-  volumeLabel.textContent = `Volume: ${Math.round(volume * 100)}%`;
-}
+const updateVolume = () => {
+    const volume = volumeControl.value;
+    music.volume = volume;
+    volumeLabel.textContent = `Volume: ${Math.round(volume * 100)}%`;
+};
 
-function getRandomIndex() {
-  let index;
-  do {
-    index = Math.floor(Math.random() * playButtons.length);
-  } while (index === currentTrackIndex);
-  return index;
-}
+const getRandomIndex = () => {
+    let index;
+    do {
+        index = Math.floor(Math.random() * playButtons.length);
+    } while (index === currentTrackIndex);
+    return index;
+};
